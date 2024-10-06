d1b780a5977a45dbed20c78722053d6d4a6d9027
Ravi Riley <raviriley@gmail.com>
2024-10-05 20:27:16 -0700

    Add pins data table

--- app/pins/page.tsx
+++ app/pins/page.tsx
@@ -1,3 +1,12 @@
-export default function Pins() {
-  return <div>All Pins</div>;
+import { DataTable } from "@/components/data-table/data-table";
+import { columns } from "./columns";
+import { fetchPins } from "@/lib/fetchPins";
+
+export default async function Pins() {
+  const pins = await fetchPins();
+  return (
+    <div className="p-4">
+      <DataTable columns={columns} data={pins} />
+    </div>
+  );
 }

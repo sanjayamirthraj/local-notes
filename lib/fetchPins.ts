33dc2f4aef1fced337196360ac21f56857bd6399
Ravi Riley <raviriley@gmail.com>
2024-10-05 20:28:12 -0700

    Create fetch pins server action

---
+++ lib/fetchPins.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { sql } from "@vercel/postgres";
+import { Pin } from "@/lib/types";
+// import { revalidatePath } from "next/cache";
+
+export const fetchPins = async (): Promise<Pin[]> => {
+  try {
+    const result = await sql`
+    SELECT latitude, longitude, message FROM location_data;
+    `;
+    const pins = result.rows.map((row) => ({
+      lat: parseFloat(row.latitude),
+      lng: parseFloat(row.longitude),
+      message: row.message,
+    }));
+    // revalidatePath("/");
+    // revalidatePath("/pins");
+    return pins;
+  } catch (error) {
+    console.error("Error fetching pins:", error);
+    return [];
+  }
+};
